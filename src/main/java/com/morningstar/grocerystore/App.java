0562ef4d18b93f46488240c776c6119ff11857a9
Phoenix Huang <PHOENIX.HUANG@morningstar.com>
2013-11-26 19:58:09 +0800

    fix a display issue

--- src/main/java/com/morningstar/grocerystore/App.java
+++ src/main/java/com/morningstar/grocerystore/App.java
@@ -26,7 +26,7 @@ public class App {
 			Dispatcher dispatcher = new Dispatcher();
 			int time = dispatcher.run(input);
 			System.out.println(String
-					.format("Finished at: t= {} minutes", time));
+					.format("Finished at: t= {0} minutes", time));
 
 		} catch (Exception ex) {
 
